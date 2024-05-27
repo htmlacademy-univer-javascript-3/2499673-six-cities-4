6f83870f0737c35e5bb8c7780a56dcce01cfa7b7
Yuichir0 <yuichir0@mail.ru>
2024-05-27 23:40:18 +0300

    7.15

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,7 @@ import { typeOfCardList } from '../utils';
 import { useAppSelector } from '../hooks';
 import CityList from '../components/city-list';
 import CardsSortingOptions from '../components/cards-sorting';
+import Header from '../components/header';
 
 export default function Main(): JSX.Element {
   const [city, offers] = useAppSelector((state) => [state.city, state.offers]);
@@ -13,36 +14,7 @@ export default function Main(): JSX.Element {
   const favoriteOffers = offers.filter((offer) => offer.isFavorite);
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <Link to="/favorites">
-                      <span className="header__favorite-count">{favoriteOffers.length}</span>
-                    </Link>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
+      <Header/>
 
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
