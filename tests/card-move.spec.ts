7d2abae8fd67219d7c56125a0ba50182f18d6dc3
Yuichir0 <yuichir0@mail.ru>
2024-06-04 14:55:30 +0300

    tests

---
+++ tests/card-move.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+// Работоспособность перехода по карточкам (должна открываться новая страничка) 
+test('check navigation to offer page', async ({ page }) => {
+
+  await page.goto('http://localhost:5173');
+
+  await page.waitForSelector('.cities__card');
+
+  const firstCardName = await page.locator('.place-card__name').first().textContent();
+  const firstCardPrice = await page.locator('.place-card__price-value').first().textContent();
+
+  await page.locator('.place-card__name').first().click();
+
+  await page.waitForSelector('.offer__name');
+
+  const offerName = await page.textContent('.offer__name');
+  const offerPrice = await page.textContent('.offer__price-value');
+
+  expect(offerName).toBe(firstCardName);
+  expect(offerPrice).toBe(firstCardPrice);
+
+  expect(await page.isVisible('.offer__gallery')).toBe(true);
+  expect(await page.isVisible('.offer__rating')).toBe(true);
+  expect(await page.isVisible('.offer__price')).toBe(true);
+  expect(await page.isVisible('.offer__inside')).toBe(true);
+  expect(await page.isVisible('.offer__host')).toBe(true);
+  expect(await page.isVisible('.offer__description')).toBe(true);
+  expect(await page.isVisible('.reviews')).toBe(true);
+});
