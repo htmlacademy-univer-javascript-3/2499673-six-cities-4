f59f7e1b573d034565a059dc33369cee5ab31207
Yuichir0 <yuichir0@mail.ru>
2024-06-04 10:09:21 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app';
+import App from './components/app2';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { checkAuthAction, fetchFavoriteAction, fetchOffersAction } from './store/api-actions';
